dd6f607fc71898165a3004a50004ac8a88f2dedc
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-14 14:50:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('JS OK');
+
+/*
+Data una lista della spesa, 
+stampare in pagina gli elementi della lista individualmente con un ciclo while.
+*/
+
+/*
+1 - Prendo l'elemento dal DOM;
+2 - Creo un array con la lista della spesa;
+3 - Creo il ciclo while;
+4 - Stampo in pagina i nomi dei prodotti della lista;
+*/
