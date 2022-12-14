1acfa61e9d43e549f49d97c5dba5f40ff77febd0
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-14 15:32:06 +0100

    create while loop & print list elements on page

--- script.js
+++ script.js
@@ -14,10 +14,29 @@ stampare in pagina gli elementi della lista individualmente con un ciclo while.
 
 // 1 - Prendo l'elemento dal DOM;
 const products = document.getElementById('products');
-console.log(products);
+////console.log(products);
 
 // 2 - Creo un array con la lista della spesa;
 const shoppingList = [
 'pane', 'uova', 'formaggio', 'latte', 'acqua'
 ];
-console.log(shoppingList.length);
+////console.log(shoppingList.length);
+
+// 3 - Creo il ciclo while;
+let myList = '<ul>';
+let i = 0;
+while (i < shoppingList.length) {
+    ////console.log(shoppingList[i]);
+    myList += `<li>${shoppingList[i]}</li>`;
+    
+    // Incremento dell'indice
+    i++;
+}
+myList += '</ul>';
+
+// 4 - Stampo in pagina i nomi dei prodotti della lista;
+products.innerHTML = myList;
+
+
+
+
